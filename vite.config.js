e0706941dca65d21388111a247e272980e231888
mdaleardc <ismailaliamot@gmail.com>
2024-12-28 20:11:17 +0600

    some updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://ismaildevcode.netlify.app', // Replace with your domain
+      routes: ['/'], // Add your site routes here
+    }),
+  ],
+});
